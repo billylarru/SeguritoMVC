75826b39c43f52d47abfd110f150f31a44ecfe05
Billy Larru <larru91@gmail.com>
2019-09-03 12:30:51 -0500

    handlebars de ejemplo

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,18 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 const express = require('express')
 const app = express()
 const router = require('./routes')
+var exphbs  = require('express-handlebars');
 
-admin.initializeApp(functions.config().firebase);
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 app.use('/', router)
 
