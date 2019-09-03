ad9a921abffb058cb1cee8c208ba6d6a1439d582
Billy Larru <larru91@gmail.com>
2019-09-03 12:51:56 -0500

    enviando data al handlebars

--- functions/index.js
+++ functions/index.js
@@ -11,10 +11,6 @@ var exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
 app.use('/', viewsRouter)
 app.use('/api', apiRouter)
 
--- functions/routes/products.routes.js
+++ functions/routes/products.routes.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const ProductsService = require('./../services/products')
+
+const productService = new ProductsService()
 
 router.get('/hello', (request, response) => {
   response.render('home')
+})
+
+
+router.get('/', async (request, response, next) => {
+  try {
+    const products = await productService.getProducts()
+    response.render('products', {  products })
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 
